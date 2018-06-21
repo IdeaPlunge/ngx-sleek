a7b1551176bea3bd9f5d761183de90a7cf88bc70
vikram <vikram@ideaplunge.in>
2018-06-21 12:23:32 +0530

    feat(initial-commit): adding table component

--- src/module/lib.module.ts
+++ src/module/lib.module.ts
@@ -1,19 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { NgModule, ModuleWithProviders } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
+// Export module's public API
+import { NgxSearchComponent } from './grid/search/search.component';
 import { NgxGridComponent } from './grid/grid.component';
 import { NgxGridService } from './grid/grid.service';
 
-// Export module's public API
-export { NgxGridComponent } from './grid/grid.component';
-export { NgxGridService } from './grid/grid.service';
-
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    FormsModule,
+    FlexLayoutModule
   ],
-  exports: [NgxGridComponent],
-  declarations: [NgxGridComponent]
+  exports: [
+    NgxGridComponent,
+    NgxSearchComponent
+  ],
+  declarations: [
+    NgxGridComponent,
+    NgxSearchComponent
+  ]
 })
 export class LibModule {
   static forRoot(): ModuleWithProviders {
